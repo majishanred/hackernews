43b530a006a87600dc6db94fb50556b27750dde1
Матвей Калашников <kmgpmain@gmail.com>
2024-07-15 02:17:23 +0200

    wip(News List): добавил styled компонент StyledCommentaryBlock

---
+++ src/styled/StyledCommentaryBlock.tsx
@@ -0,0 +1,14 @@
+import {Stack, styled} from "@mui/material";
+
+const StyledCommentaryBlock = styled(Stack)`
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    margin-left: 32px;
+    
+    width: 100%;
+    height: auto;
+    padding: 8px 16px;
+`;
+
+export default StyledCommentaryBlock;
