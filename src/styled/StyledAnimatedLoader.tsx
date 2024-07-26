8e9ea83d541943b764e9019dd200269980a38485
Матвей Калашников <kmgpmain@gmail.com>
2024-07-26 20:32:27 +0700

    pr(): поправил замечания

--- src/styled/StyledAnimatedLoader.tsx
+++ src/styled/StyledAnimatedLoader.tsx
@@ -7,7 +7,7 @@ const StyledAnimatedLoader = styled(Loop)`
   margin: 12px auto;
   animation-name: logo-spin;
   animation-duration: 5s;
-  animation-timing-function: cubic-bezier(0.18, 1.01, 0.88, 0.15); //cubic-bezier(0.2, 0.88, 0.68, 0.19);
+  animation-timing-function: cubic-bezier(0.18, 1.01, 0.88, 0.15);
   animation-delay: 0s;
   animation-iteration-count: infinite;
 `;
