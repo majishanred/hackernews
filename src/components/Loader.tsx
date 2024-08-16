333bcfc54b3573219f225cf908fe979d0f94f78b
Матвей Калашников <kmgpmain@gmail.com>
2024-08-16 19:12:49 +0700

    pr: исправлены заменчания в лоадере

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,21 +1,22 @@
-import { Stack, styled, Typography } from '@mui/material';
+import { styled, Typography } from '@mui/material';
 import { StyledLoopAnimation } from '../styled/StyledLoopAnimation.tsx';
+import StyledOverlapingContainer from '../styled/StyledOverlapingContainer.ts';
 
 const Loader = () => {
   return (
-    <Stack textAlign="center" marginY={3}>
+    <StyledOverlapingContainer>
       <Typography variant="body1" marginX="auto">
         Грузим данные, подождите немного...
       </Typography>
       <StyledAnimatedLoader />
-    </Stack>
+    </StyledOverlapingContainer>
   );
 };
 
 const StyledAnimatedLoader = styled(StyledLoopAnimation)`
   width: 64px;
   height: 64px;
-  margin: ${({ theme }) => theme.spacing(2, 'auto')};
+  margin: ${({ theme }) => theme.spacing(2, 0, 2, 0)};
   animation-name: logo-spin;
   animation-duration: 5s;
   animation-timing-function: cubic-bezier(0.18, 1.01, 0.88, 0.15);
