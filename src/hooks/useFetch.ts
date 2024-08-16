891ac45d230aa5d8fa3fcb8b3668e4e08d4bdd2e
Матвей Калашников <kmgpmain@gmail.com>
2024-08-16 23:58:00 +0700

    pr: исправлена типизация useFetcher

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,15 +2,16 @@ import { useDispatch } from 'react-redux';
 import { useErrorBoundary } from 'react-error-boundary';
 import { useEffect } from 'react';
 import { CanceledError } from 'axios';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 
 //@Note: вот это надо типизировать покруче, я знаю что эта штука в Суте есть, но туда за ней лезть не хочу, хочу свою.
 export const useFetcher = <T, U extends unknown[]>(
   fetchFunc: (args: U, signal: AbortSignal) => Promise<T>,
   fetchFuncArgs: U,
   refetchInterval: number,
-  saveData: any,
-  toggleLoading: any,
-  refetchIndicator: any,
+  saveData: ActionCreatorWithPayload<T>,
+  toggleLoading: ActionCreatorWithPayload<boolean>,
+  refetchIndicator: number,
 ) => {
   const dispatch = useDispatch();
   const { showBoundary } = useErrorBoundary();
@@ -22,7 +23,6 @@ export const useFetcher = <T, U extends unknown[]>(
       dispatch(toggleLoading(true));
       try {
         const data = await fetchFunc(fetchFuncArgs, controller.signal);
-        console.log(data);
         dispatch(saveData(data));
         dispatch(toggleLoading(false));
       } catch (error) {
